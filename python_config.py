68596ed64169757046a3848a01838faad6325eee
Adriano Margarin <adriano.margarin@gmail.com>
2017-08-29 21:57:18 -0300

    Bugfix variables split

--- python_config.py
+++ python_config.py
@@ -5,7 +5,7 @@ def get_variables_from_env():
     env_vars = {}
     with open(".env") as env:
         for variables in env:
-            env_vars[variables.split('=')[0]] = variables.split('=')[1]
+            env_vars[variables.split('=', 1)[0]] = variables.split('=', 1)[1]
     return env_vars
 
 
